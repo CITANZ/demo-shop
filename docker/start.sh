c50e99c15fad9f660a34596d0fe1e0917f2c1f04
luhao <yangluhao123@gmail.com>
2020-03-21 16:26:34 +1300

    put docoker related files in one place

---
+++ docker/start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+initialised="/tmp/initialised"
+if [ ! -f $initialised ]
+	echo "init..."
+	# replace apache's default config /var/www/html to /var/www/html/public
+	sed -i -e "s/\/var\/www\/html/\/var\/www\/html\/public/g" /etc/apache2/sites-available/000-default.conf 
+	# suppress the warning: Could not reliably determine the server's fully qualified domain name, using 192.168.80.2. Set the 'ServerName' directive globally 
+	echo "ServerName localhost" >> /etc/apache2/apache2.conf
+	# restart apache
+	apache2ctl stop && apache2ctl -D FOREGROUND
+	touch $initialised
+then
+	echo "skipping init"
+fi
+
+# this is a signal whether composer update has been done
+file="/tmp/composer-updated"
+
+if [ ! -f $file ]
+	echo "doing composer update..."
+	cd /var/www/html
+	composer update
+	touch $file
+then
+	echo "skipping composer update"
+fi
+
+
+
