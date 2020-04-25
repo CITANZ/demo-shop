956e12beda24937889024a9edd612d193050bf93
Leo Chen <leo@leochen.co.nz>
2020-04-26 00:20:12 +1200

    reflect the latest change

--- frontend/src/components/blocks/CartItemForm.vue
+++ frontend/src/components/blocks/CartItemForm.vue
@@ -2,8 +2,18 @@
 <form method="post" @submit.prevent="update_qty">
     <fieldset :disabled="is_working">
         <div class="columns">
-            <div class="column">{{item.product.title}}</div>
+            <div class="column">
+                <p><span v-if="isBundle">[BUNDLED] </span>{{item.product.title}}</p>
+                <div class="content" v-if="isBundle">
+                    <ul>
+                        <li v-for="variant in item.product.variants">{{ variant.title }} x 1 ({{ variant.price.toDollar() }})</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="column is-2" v-if="item.product.special_price">{{ item.product.special_price.toDollar() }}</div>
+            <div class="column is-2" v-else>{{ item.product.price_label }}</div>
             <div class="column is-2"><input @change="update_qty" type="number" class="input" v-model="item.quantity" /></div>
+            <div class="column is-1">{{ item.subtotal.toDollar() }}</div>
             <div class="column is-narrow"><a @click.prevent="delete_item" class="button is-danger">Delete</a></div>
         </div>
     </fieldset>
@@ -19,6 +29,12 @@ export default {
         return {
             is_working  :   false
         };
+    },
+    computed: {
+        isBundle() {
+            if (!this.item) return false;
+            return this.item.product.variants && this.item.product.variants.length;
+        }
     },
     methods     :   {
         update_qty()
--- frontend/src/components/pages/CartPage.vue
+++ frontend/src/components/pages/CartPage.vue
@@ -3,7 +3,24 @@
     <div class="container">
         <h1 class="title is-2">{{site_data.title}}</h1>
         <div class="cart-content" v-if="site_data.cart">
+            <template v-if="site_data.cart.messages && site_data.cart.messages.length">
+                <div class="notification is-warning" v-for="message in site_data.cart.messages">
+                    <button class="delete"></button>
+                    <div v-html="message"></div>
+                </div>
+            </template>
             <table class="is-fullwidth table">
+                <tr>
+                    <td>
+                        <div class="columns">
+                            <div class="column"><strong>Product</strong></div>
+                            <div class="column is-2"><strong>Unit Price</strong></div>
+                            <div class="column is-2"><strong>Quantity</strong></div>
+                            <div class="column is-1"><strong>Total</strong></div>
+                            <div class="column is-narrow"><a class="button is-danger is-invisible">Delete</a></div>
+                        </div>
+                    </td>
+                </tr>
                 <tr v-for="item in site_data.cart.items">
                     <td><cart-item :item="item" /></td>
                 </tr>
